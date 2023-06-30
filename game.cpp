65dfa1ba162c2b10f7ca7e75ac710069d9ecdbb1
Frederick <Frederickdsoke10@outlook.com>
2023-06-30 00:37:26 -0400

    added skeleton makefile, game file, test file

---
+++ game.cpp
@@ -0,0 +1,13 @@
+#include "shark.h"
+#include "Map.h"
+#include "locations.h"
+#include "events.h"
+
+
+class Sharklife {
+
+//methods for running the game
+//birth the shark at the start of the game
+
+//storage of the game
+};
