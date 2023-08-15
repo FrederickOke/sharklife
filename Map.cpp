6620b9d6f513d2f05dd9170a6ff0338933323612
Frederick <Frederickdsoke10@outlook.com>
2023-08-15 19:53:28 -0400

    modified tree traversal helper function

--- Map.cpp
+++ Map.cpp
@@ -1,15 +1,19 @@
 #include "Map.h"
 
+Location* Map::recInsert(Location* curr, Location* toInsert){
+
+}
+
 void Map::insertLocation(Location* location){
+    
     //make sure node is not in the tree
     if (findLocation(location->getName()) != nullptr){
         cout << "Location already in tree. Insertion terminated." << endl;
     }
-    else {
-        
+    else { //location is not a duplicate and can be inserted into tree
+
     }
 }
-
 
 //desc: returns nullptr if target not in the tree. else returns a pointer to that location
 Location* Map::findLocation(string targetName){
@@ -18,33 +22,57 @@ Location* Map::findLocation(string targetName){
         return nullptr;
     }
     else {
-        return recFind(targetName, m_root); //RETURNS EITHER NULLPTR (if not in tree) or a pointer pointing to the node with the name
+        return recFindIns(targetName, m_root, FINDMODE); //RETURNS EITHER NULLPTR (if not in tree) or a pointer pointing to the node with the name in find mode
     }
 }
 
-Location* Map::recFind(string targetName, Location* curr){
-    //BASE CASE
-    if (curr->getName() == targetName){ //if a node w/ same name is in the tree
-        return curr;
-    }
+Location* Map::recFindIns(string targetName, Location* curr, int mode){
+    
+    if (mode == FINDMODE){
+            //BASE CASE
+        if (curr->getName() == targetName){ //if a node w/ same name is in the tree
+            return curr;
+        }
 
-    //RECURSIVE CASES
-    else if (curr->getName() < targetName){ //if target node is higher in ascii value
-        if (curr->getRight() == nullptr) //if there is no right child return nullptr
-            return nullptr;
-        else { //return the recursive result of the right subtree
-            return recFind(targetName, curr->getRight());
+        //RECURSIVE CASES
+        else if (curr->getName() < targetName){ //if target node is higher in ascii value
+            if (curr->getRight() == nullptr) //if there is no right child return nullptr
+                return nullptr;
+            else { //return the recursive result of the right subtree
+                return recFindIns(targetName, curr->getRight(), FINDMODE);
+            }
+        }
+        else { //target node is lower in ascii value
+            if (curr->getLeft() == nullptr)
+                return nullptr;
+            else { //return the recursive result of the left subtree
+                return recFindIns(targetName, curr->getLeft(), FINDMODE);
+            }
         }
     }
-    else { //target node is lower in ascii value
-        if (curr->getLeft() == nullptr)
+    if (mode == INSERTMODE){
+            //BASE CASE
+        if (curr->getName() == targetName){ //if a node w/ same name is in the tree
             return nullptr;
-        else { //return the recursive result of the left subtree
-            return recFind(targetName, curr->getLeft());
+        }
+
+        //RECURSIVE CASES
+        else if (curr->getName() < targetName){ //if target node is higher in ascii value
+            if (curr->getRight() == nullptr) //if there is no right child return this node
+                return curr;
+            else { //return the recursive result of the right subtree
+                return recFindIns(targetName, curr->getRight(), INSERTMODE);
+            }
+        }
+        else { //target node is lower in ascii value
+            if (curr->getLeft() == nullptr)
+                return curr;
+            else { //return the recursive result of the left subtree
+                return recFindIns(targetName, curr->getLeft(), INSERTMODE);
+            }
         }
     }
 }
-
 
 bool Map::isEmpty(){
     if (m_root == nullptr){
--- Map.h
+++ Map.h
@@ -3,6 +3,9 @@
 using namespace std;
 
 
+const int FINDMODE = 0;
+const int INSERTMODE = 1;
+
 class Map {
     public:
     // Name: Map() - Default Constructor
@@ -33,7 +36,14 @@ class Map {
 
     //finding methods: no duplicates allowed in the tree
     Location * findLocation(string targetName); //check if a location by the same name is in the tree
-    Location * recFind(string targetName, Location* curr); //recursively look through the tree for target
+    
+    //name: recFind
+    //desc: tree traversal helper function
+    //  FINDMODE: check if a location is in the tree recursively. if it is, return the location pointer. if it is not, return nullptr
+    //  INSERTMODE: for insertion - check if a location is in the tree recursively. if it is, return nullptr. 
+    //         if it is not, return a location pointer to what it's parent would be if it was in the tree.
+    //         only used if the tree is non-empty
+    Location * recFindIns(string targetName, Location* curr, int mode); //recursively look through the tree for target
     
     //rebalancing methods
     bool isEmpty(); //whether the tree is empty
